83b1ac92c7f86779c5e3b1d3b33e07aeb42870e1
Gilles Delpy SOP <165911061+Ward0007@users.noreply.github.com>
2025-04-12 10:32:19 +0200

    Update scrape_intraday.sh

--- scrape_intraday.sh
+++ scrape_intraday.sh
@@ -6,15 +6,15 @@ URL="https://www.boursedirect.fr/api/instrument/intraday/XPAR/PX1/EUR"
 response=$(curl -s -H "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0" $URL)
 
 if [[ $? -ne 0 ]]; then
-  echo "API connection error"  #check if the request was successful
+  echo "API connection error"
   exit 1
 fi
 
-if [[ "$response" == *"403 Forbidden"* ]]; then  #Check if the server returns a 403 error
+if [[ "$response" == *"403 Forbidden"* ]]; then
   echo "Erreur 403 : Accès interdit à l'API. Vérifiez les permissions ou les en-têtes de la requête."
   exit 1
 fi
-#Save raw json to file for verification
+# Save raw json to file for verification
 echo "$response" > raw_data.json
 
 # Extraction using Regex and csv file creation
@@ -26,10 +26,10 @@ echo "$response" | grep -oP '"Date":\s*\K[0-9]+' | while read -r date; do
     highPrice=$(echo "$response" | grep -oP "(\"Date\":\s*$date,.*?\"High\":\s*\K[0-9.]+)")
     lowPrice=$(echo "$response" | grep -oP "(\"Date\":\s*$date,.*?\"Low\":\s*\K[0-9.]+)")
     
-    #for a readable date format
+    # Convert timestamp to readable date format
     formattedDate=$(date -d @$date +"%Y-%m-%d %H:%M:%S")
 
-        # On récupère l'heure au format HHMM, par ex. 1730
+    # On récupère l'heure au format HHMM, par ex. 1730
     hourCheck=$(date -d @"$date" +%H%M)
 
     # Vérifier si l'heure est <= 17h30 (1730)
