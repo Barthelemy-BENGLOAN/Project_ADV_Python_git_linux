fcc0f46b2073e269673217e44bf4735878514b16
Gilles Delpy SOP <165911061+Ward0007@users.noreply.github.com>
2025-04-12 12:07:26 +0200

    Update scrape_intraday.sh

--- scrape_intraday.sh
+++ scrape_intraday.sh
@@ -2,6 +2,7 @@
 
 # URL de l'API pour les données du CAC40
 URL="https://www.boursedirect.fr/api/instrument/intraday/XPAR/PX1/EUR"
+export TZ="Europe/Paris"  # Pour définir le fuseau horaire à GMT+2 (France)
 
 # Requête GET avec en-têtes HTTP pour simuler un navigateur
 response=$(curl -s -H "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0" $URL)
